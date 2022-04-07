11ff992f6dbabc4ad6d981ce67c8f90633b65410
Dmitrii <ssdmitrii28@gmail.com>
2022-04-07 22:10:29 +0700

    Output of three interfaces (task 2.7, 2.8, 2.9) in one main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "win9.h"
+#include "win8.h"
+#include "win.h"
 #include <QApplication>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Win9 w;
+    Win9 w9;
+    Win8 w8;
+    Win w;
     w.move(100,150);//сдвигаем окно интерфейса на позицию 100,150
     w.show();
+    w8.move(400,150);//сдвигаем окно интерфейса на позицию 400,150
+    w8.show();
+    w9.move(700,150);//сдвигаем окно интерфейса на позицию 700,150
+    w9.show();
     return a.exec();
 }
